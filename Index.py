bb3c068ea2cb0fdab2362b84d74928331d731a4b
Darwin Hianpier Maravi Loyola <dmaraviloyola709@gmail.com>
2026-03-22 23:32:49 -0500

    Sincronizando footer y about final

--- Index.py
+++ Index.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Ruta principal (Inicio)
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Ruta para la página About (fíjate que está en minúsculas)
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Condición para arrancar el servidor (sin espacios extras)
+@app.route('/servicios')
+def servicios():
+    return "Sección de Servicios en construcción"
+
+@app.route('/contacto')
+def contacto():
+    return "Sección de Contacto en construcción"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Cambiamos a puerto 5001 para evitar bloqueos
+    app.run(debug=True, port=5001)
